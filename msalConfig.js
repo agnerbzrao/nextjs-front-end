2202f4d7ee5da3e13a4869364cd80e0498b21283
Agner Souza Bezerra <agner.bezerra@toledobrasil.com>
2025-02-14 10:35:48 -0300

    Começo da inserção do login via Microsoft Entra Id.

---
+++ msalConfig.js
@@ -0,0 +1,46 @@
+import { LogLevel } from "@azure/msal-browser";
+
+/**
+ * Configuration object to be passed to MSAL instance on creation.
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+ */
+
+export const msalConfig = {
+  auth: {
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID_CIAM || "", // This is the ONLY mandatory field that you need to supply.
+    authority: process.env.NEXT_PUBLIC_AUTHORITY_CIAM || "", // Replace the placeholder with your tenant subdomain
+    redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI_CIAM || "", // Points to window.location.origin. You must register this URI on Microsoft Entra admin center/App Registration.
+    postLogoutRedirectUri: "/", // Indicates the page to navigate after logout.
+    navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
+  },
+  cache: {
+    cacheLocation: "sessionStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            return;
+          case LogLevel.Info:
+            console.info(message);
+            return;
+          case LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case LogLevel.Warning:
+            console.warn(message);
+            return;
+          default:
+            return;
+        }
+      },
+    },
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.scss";
+import { useMsal, useIsAuthenticated } from "@azure/msal-react";
+import AZURE_BLOB_URL from "@/app/config/blob";
+
 export default function LoginPage() {
+  const { instance } = useMsal();
+  const isAuthenticated = useIsAuthenticated();
+  const activeAccount = instance.getActiveAccount();
+
+  const handleLoginMsal = () => {
+    instance
+      .loginRedirect({
+        prompt: "create",
+        scopes: [],
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
   return (
-    <>
+    <div className={styles.containerLogin}>
       <div className={styles.leftColumn}>
         <Image
-          src={`${process.env.NEXT_PUBLIC_AZURE_BLOB_URL}/front-end-images/login-page/bg_login.svg`}
+          src={`${AZURE_BLOB_URL}/front-end-images/login-page/bg_login.svg`}
           alt="multiplos-triangulos"
           width={0}
           height={0}
@@ -15,17 +34,21 @@ export default function LoginPage() {
       <div className={styles.rightColumn}>
         <div className={styles.rightContent}>
           <Image
-            src={`${process.env.NEXT_PUBLIC_AZURE_BLOB_URL}/front-end-images/login-page/parceiroPrixLogo.svg`}
+            src={`${AZURE_BLOB_URL}/front-end-images/login-page/parceiroPrixLogo.svg`}
             alt="parceiro-prix-logo"
             width={0}
             height={0}
             className={styles.imgParceiroPrixLogo}
           ></Image>
-          <button type="button" className={styles.buttonlogin}>
+          <button
+            type="button"
+            className={styles.buttonlogin}
+            onClick={handleLoginMsal}
+          >
             <span className={styles.jaSouParceiro}>Já sou parceiro Prix.</span>
             <span className={styles.entrar}>Entrar.</span>
             <Image
-              src={`${process.env.NEXT_PUBLIC_AZURE_BLOB_URL}/front-end-images/login-page/exit_icon.svg`}
+              src={`${AZURE_BLOB_URL}/front-end-images/login-page/exit_icon.svg`}
               alt="exit-icon"
               width={0}
               height={0}
@@ -36,7 +59,7 @@ export default function LoginPage() {
           <p className={styles.dificuldadesAcesso}>
             Está com dificuldades no seu acesso?&nbsp;
             <a
-              href={`${process.env.NEXT_PUBLIC_AZURE_BLOB_URL}/documentos/tutorial-acesso-porta-parceiro-prix.pdf`}
+              href={`${AZURE_BLOB_URL}/documentos/tutorial-acesso-porta-parceiro-prix.pdf`}
               target="_blank"
               rel="noreferrer"
             >
@@ -48,12 +71,12 @@ export default function LoginPage() {
             href="https://www.toledobrasil.com/seja-um-parceiro"
             target="_blank"
             rel="noreferrer"
-            style={{ textDecorationLine: "none" }}
+            className={styles.textNotDecorated}
           >
             <button type="button" className={styles.buttonQueroSerParceiro}>
               <span>Quero ser um parceiro Prix</span>
               <Image
-                src={`${process.env.NEXT_PUBLIC_AZURE_BLOB_URL}/front-end-images/login-page/star_icon.svg`}
+                src={`${AZURE_BLOB_URL}/front-end-images/login-page/star_icon.svg`}
                 alt="star-icon"
                 width={0}
                 height={0}
@@ -69,6 +92,6 @@ export default function LoginPage() {
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
